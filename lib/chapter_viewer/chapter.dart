7ca3a4c689e52b033517d6802e2b09dc514e3e23
Akiva Zonenfeld <akivazo1@gmail.com>
2024-12-27 13:29:01 +0200

    slide

--- lib/chapter_viewer/chapter.dart
+++ lib/chapter_viewer/chapter.dart
@@ -54,42 +54,62 @@ class _ChapterViewerState extends State<ChapterViewer> {
     }
   }
 
+  void _onHorizontalDragEnd(DragEndDetails details) {
+    // Check the velocity of the swipe
+    if (details.primaryVelocity != null) {
+      if (details.primaryVelocity! > 0) {
+        // Swipe right
+        setState(() {
+          toggleSectionBackward();
+        });
+      } else if (details.primaryVelocity! < 0) {
+        // Swipe left
+        setState(() {
+          toggleSectionForward();
+        });
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        leading: IconButton(onPressed: () {
-          Navigator.of(context).push(MaterialPageRoute(builder: (context) => AppEntrance()));
-        }, icon: Icon(Icons.home)),
-        centerTitle: true,
-        title: Text(currentSubChapter.title,),
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          SizedBox(height: 20,),
-          Expanded(child: SafeArea(child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: currentSection,
-          ))),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              IconButton(onPressed: () {
-                setState(() {
-                  toggleSectionBackward();
-                });
-              }, icon: Icon(Icons.arrow_back)),
-              IconButton(onPressed: () {
-                setState(() {
-                  toggleSectionForward();
-                });
-              }, icon: Icon(Icons.arrow_forward))
-            ],
-          )
+    return GestureDetector(
+      onHorizontalDragEnd: _onHorizontalDragEnd,
+      child: Scaffold(
+        appBar: AppBar(
+          leading: IconButton(onPressed: () {
+            Navigator.of(context).push(MaterialPageRoute(builder: (context) => AppEntrance()));
+          }, icon: Icon(Icons.home)),
+          centerTitle: true,
+          title: Text(currentSubChapter.title,),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            SizedBox(height: 20,),
+            Expanded(child: SafeArea(child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: currentSection,
+            ))),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                IconButton(onPressed: () {
+                  setState(() {
+                    toggleSectionBackward();
+                  });
+                }, icon: Icon(Icons.arrow_back)),
+                IconButton(onPressed: () {
+                  setState(() {
+                    toggleSectionForward();
+                  });
+                }, icon: Icon(Icons.arrow_forward))
+              ],
+            )
 
-        ],
+          ],
+        ),
       ),
     );
   }
